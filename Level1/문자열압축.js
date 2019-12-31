80f81c869f91749804a9f7cb1d14008b9b243182
JunYoung <jun094@naver.com>
2019-12-31 15:52:43 +0900

    2020 카카오코테 1번 _ 무한반복문 ver

--- Level1/문자열압축.js
+++ Level1/문자열압축.js
@@ -2,12 +2,32 @@ let unit = 3; // 단위
 
 function compression(s) {
   let len = s.length;
-  let count = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s.substr(i, unit) == s.substr(i + unit, unit)) {
-      s = s.substr(i + unit, unit) + s.substr(i + unit, unit);
-      console.log(s);
+  let answer;
+  let cnt = 0;
+
+  if (len === 1) {
+    answer = 1;
+  } else {
+    for (let i = 0; i <= s.length - unit; i++) {
+      let checkString = s.substr(i, unit); // abc, bca, .., def
+
+      let j = i + unit; //j=3, j=4, ..., j=9
+      while (true) {
+        if (j >= len) {
+          break;
+        }
+        if (checkString == s.substr(j, unit)) {
+          cnt++; // 3
+          j = j + unit; // j=15
+
+          console.log(checkString);
+        } else {
+          break;
+        }
+      }
     }
   }
+  console.log(cnt);
 }
+
 compression("abcabcdefdefdef");
