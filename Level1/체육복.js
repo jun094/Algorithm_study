d54bc1155bf0c7b1b67960e482da91cf62b294d6
JunYoung <jun094@naver.com>
2020-01-08 23:55:50 +0900

    level 1

--- Level1/체육복.js
+++ Level1/체육복.js
@@ -2,7 +2,7 @@
 
 let n = 5;
 let lost = [2, 4];
-let reserve = [3];
+let reserve = [1, 3, 5];
 
 // lost길이만큼 순회-> lost배열 pop하면서 가능하면 해당원소 resrve배열에서 제거
 //                             불가능하면 n--;
@@ -12,29 +12,16 @@ function solution(n, lost, reserve) {
 
   for (let i = 0; i < lost.length; i++) {
     let check = lost[i];
-
-    reserve.some(have => {
-      console.log("없는사람 " + check);
-      console.log("여벌있는사람 " + have);
-
-      if (check - 1 === have) {
-        let c = reserve.splice(reserve.indexOf(have), 1);
-        console.log("-", reserve);
-        return check - 1 === have;
-      } else if (check + 1 === have) {
-        let c = reserve.splice(reserve.indexOf(have), 1);
-        console.log("+", reserve);
-        return check + 1 === have;
-      } else {
-        console.log("!");
-        answer = answer - 1;
-      }
-
-      console.log("============");
+    console.log(check);
+    let c = false;
+    c = reserve.some(have => {
+      return check - 1 === have || check + 1 === have;
     });
+
+    console.log(check, c);
   }
   //console.log(reserve);
-  console.log(answer);
+  //console.log(answer);
 }
 
 solution(n, lost, reserve);
