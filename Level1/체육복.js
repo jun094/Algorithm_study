633163c8b6c18b6e30bdbb437d8f3b2ab01a5118
JunYoung <jun094@naver.com>
2020-01-09 18:14:11 +0900

    Level 1

--- Level1/체육복.js
+++ Level1/체육복.js
@@ -1,28 +1,50 @@
 /** @format */
 
-let n = 5;
-let lost = [2, 4];
-let reserve = [1, 3, 5];
+let n = 10;
+let lost = [3, 9, 10];
+let reserve = [3, 8, 9];
 
-// lost길이만큼 순회-> lost배열 pop하면서 가능하면 해당원소 resrve배열에서 제거
+// 탐욕법 -> reserve는 전체를 돌지 않는 것이 중요 !!
+//        -> lost와 같은게 있을 때마다 for문 탈출
+// lost길이만큼 s순회-> lost배열 pop하면서 가능하면 해당원소 resrve배열에서 제거
 //                             불가능하면 n--;
 
 function solution(n, lost, reserve) {
   var answer = n;
-
-  for (let i = 0; i < lost.length; i++) {
-    let check = lost[i];
-    let c = false;
-
-    c = reserve.some(have => {
-      reserve.splice(reserve.indexOf(have), 1);
-      return check - 1 === have || check + 1 === have;
-    });
-
-    console.log(check, c);
+  let total = [];
+  for (let i = 0; i < n; i++) {
+    total[i] = 1;
   }
-  //console.log(reserve);
-  //console.log(answer);
+  for (let i = 0; i < reserve.length; i++) {
+    total[reserve[i] - 1] = 2;
+  }
+  for (let i = 0; i < lost.length; i++) {
+    if (total[lost[i] - 1] === 2) {
+      total[lost[i] - 1] = 1;
+    } else {
+      total[lost[i] - 1] = 0;
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (total[i] === 0 && total[i - 1] == 2) {
+      total[i] = 1;
+      total[i - 1] = 1;
+    } else if (total[i] === 0 && total[i + 1] == 2) {
+      total[i] = 1;
+      total[i + 1] = 1;
+    }
+  }
+
+  console.log(total);
+  for (let i = 0; i < n; i++) {
+    if (total[i] === 0) {
+      answer--;
+    }
+  }
+
+  console.log(answer);
+  return answer;
 }
 
 solution(n, lost, reserve);
