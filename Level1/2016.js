5aa1ec449234b1ee29e08031ed6740a891ac6703
JunYoung <jun094@naver.com>
2020-01-13 21:40:13 +0900

    Level 2

--- Level1/2016.js
+++ Level1/2016.js
@@ -1,6 +1,22 @@
 /** @format */
 
-function solution(a, b) {
+function solution(m, d) {
   var answer = "";
+  let month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let dayWeek = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+  let totalDay = 0;
+
+  //totalDay구하기  ex) 3.10 -> 31+28+10
+  for (let i = 0; i < m - 1; i++) {
+    totalDay = totalDay + month[i];
+  }
+  totalDay = totalDay + d;
+
+  answer = dayWeek[totalDay % 7];
+
+  console.log(totalDay);
+  console.log(answer);
   return answer;
 }
+
+solution(5, 24);
