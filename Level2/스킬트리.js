b323757e0bfb5506145386b0052633424adfb8f2
JunYoung <jun094@naver.com>
2020-01-17 23:06:50 +0900

    Level 2

--- Level2/스킬트리.js
+++ Level2/스킬트리.js
@@ -1,34 +1,35 @@
-const skill = "CBD";
-const skill_trees =  ["BACDE", "CBADF", "AECB", "BDA"];
+const skill = 'CBD';
+const skill_trees = ['BACDE', 'CBADF', 'AECB', 'BDA'];
 
 function solution(skill, skill_trees) {
     var answer = 0;
-    let arr = skill.split("");
-    let count =0;
+    let arr = skill.split('');
+    let count = 0,
+        len,
+        leww;
 
-    skill_trees.forEach(i=>{
-        let check = i.split("");
+    skill_trees.forEach(i => {
+        let check = i.split('');
         let len = check.length;
         let flag = false;
 
-        for(let j=0;j<len;j++){
+        for (let j = 0; j < len; j++) {
             let temp = check.shift();
 
-            if(temp == arr[count]){
+            if (temp == arr[count]) {
                 count++;
             }
         }
 
-        if( flag == true ){
+        if (flag == true) {
             console.log(i);
             answer++;
         }
         count = 0;
-
-    })    
+    });
 
     console.log(answer);
     return answer;
 }
 
-solution(skill, skill_trees)
+solution(skill, skill_trees);
