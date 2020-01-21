61f87c516622312a51275e1d667f477a7e871c86
JunYoung <jun094@naver.com>
2020-01-21 17:24:35 +0900

    Level2

--- Level2/프린터.js
+++ Level2/프린터.js
@@ -3,49 +3,49 @@ const priorities = [1, 1, 9, 1, 1, 1];
 const location = 0;
 
 function solution(priorities, location) {
-  var answer = 0;
-  let len = priorities.length;
-  let priorStack = [];
+    var answer = 0;
+    let len = priorities.length;
+    let priorStack = [];
 
-  priorities.forEach((item, index) => {
-    priorStack.push({ index: index, item: item });
-  });
+    priorities.forEach((item, index) => {
+        priorStack.push({ index: index, item: item });
+    });
 
-  console.log(priorStack);
-  console.log("========================");
+    console.log(priorStack);
+    console.log('========================');
 
-  while (len) {
-    let check = priorStack.shift();
-    let temp = 1;
-    for (let i = 0; i < priorStack.length; i++) {
-      if (check.item < priorStack[i].item) {
-        priorStack.push(check);
-        temp = 0;
-      }
+    while (len) {
+        let check = priorStack.shift();
+        let temp = 1;
+        for (let i = 0; i < priorStack.length; i++) {
+            if (check.item < priorStack[i].item) {
+                priorStack.push(check);
+                temp = 0;
+            }
+        }
+        if (temp != 0) {
+            priorStack.unshift(check);
+        }
+
+        len = len - 1;
     }
-    if (temp != 0) {
-      priorStack.unshift(check);
+
+    console.log(priorStack);
+    console.log('========================');
+    let i = 1;
+    while (1) {
+        let check = priorStack.shift();
+
+        //console.log(check.index);
+        if (check.index === location) {
+            answer = i;
+            break;
+        }
+        i++;
     }
 
-    len = len - 1;
-  }
-
-  console.log(priorStack);
-  console.log("========================");
-  let i = 1;
-  while (1) {
-    let check = priorStack.shift();
-
-    //console.log(check.index);
-    if (check.index === location) {
-      answer = i;
-      break;
-    }
-    i++;
-  }
-
-  console.log(answer);
-  return answer;
+    console.log(answer);
+    return answer;
 }
 
 solution(priorities, location);
