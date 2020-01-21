c705b53373da55c5e8b6b682576483fab7a22263
JunYoung <jun094@naver.com>
2020-01-21 17:23:25 +0900

    Level 2

--- Level2/다리를 지나는 트럭.js
+++ Level2/다리를 지나는 트럭.js
@@ -1,15 +1,42 @@
-const bridge_length = 2;
-const weight = 10;
+const bridge_length = 100;
+const weight = 100;
+const truck_weights = [10];
 
 function solution(bridge_length, weight, truck_weights) {
-    var answer = 0;
     let temp = [];
-    let time =0;
-    
-    for(let i=0;i<2;i++){
-        temp.push()
+    let time = 0;
+
+    for (let i = 0; i < bridge_length; i++) {
+        temp.push(0);
     }
-    return answer;
+
+    for (let i = 0; i < truck_weights.length; i++) {
+        temp.pop();
+        let truck = truck_weights[i];
+
+        console.log('검사항목', truck, temp);
+
+        let initValue = truck;
+        let sum = temp.reduce((initialValue, currentValue, currentIndex, array) => {
+            return initialValue + currentValue;
+        }, initValue);
+
+        console.log('배열 총합', sum);
+
+        if (sum <= weight) {
+            temp.unshift(truck);
+        } else {
+            temp.unshift(0);
+            i = i - 1;
+        }
+        console.log(temp);
+        time++;
+        console.log(time);
+    }
+    time = time + bridge_length;
+
+    console.log(time);
+    return time;
 }
 
-solution(bridge_length, weight);
+solution(bridge_length, weight, truck_weights);
