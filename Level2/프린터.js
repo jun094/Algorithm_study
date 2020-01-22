60d31971d7b30b8e29520623c3eb9052c17998a0
JunYoung <jun094@naver.com>
2020-01-22 17:47:57 +0900

    Level 2 - idea만 완성

--- Level2/프린터.js
+++ Level2/프린터.js
@@ -2,47 +2,26 @@
 const priorities = [1, 1, 9, 1, 1, 1];
 const location = 0;
 
+function checkPrior(arr, val) {
+    return arr.some(function(arrVal) {
+        console.log(arrVal, val);
+    });
+}
+
 function solution(priorities, location) {
     var answer = 0;
     let len = priorities.length;
-    let priorStack = [];
-
-    priorities.forEach((item, index) => {
-        priorStack.push({ index: index, item: item });
+    let priorQ = [];
+    priorities.forEach((prior, index) => {
+        priorQ.push({ item: index, prior: prior });
     });
+    console.log(priorQ);
 
-    console.log(priorStack);
-    console.log('========================');
-
-    while (len) {
-        let check = priorStack.shift();
-        let temp = 1;
-        for (let i = 0; i < priorStack.length; i++) {
-            if (check.item < priorStack[i].item) {
-                priorStack.push(check);
-                temp = 0;
-            }
+    priorQ.forEach(item => {
+        if (checkPrior(priorQ, item)) {
+            console.log('발견');
         }
-        if (temp != 0) {
-            priorStack.unshift(check);
-        }
-
-        len = len - 1;
-    }
-
-    console.log(priorStack);
-    console.log('========================');
-    let i = 1;
-    while (1) {
-        let check = priorStack.shift();
-
-        //console.log(check.index);
-        if (check.index === location) {
-            answer = i;
-            break;
-        }
-        i++;
-    }
+    });
 
     console.log(answer);
     return answer;
