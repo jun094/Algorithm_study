05537a76a72f178b4b21f7a3bc3872f039563114
JunYoung <jun094@naver.com>
2020-01-29 15:20:38 +0900

    StringBuilder 추가

--- 탐욕법/큰수만들기.js
+++ 탐욕법/큰수만들기.js
@@ -1,26 +1,82 @@
 const number = '1924';
 const k = 2;
 
+/////////////////////
+var StringBuilder = function() {
+    this.buffer = new Array();
+};
+
+//순서대로 문자열을 추가한다.
+
+StringBuilder.prototype.Append = function(strValue) {
+    this.buffer[this.buffer.length] = strValue;
+};
+
+//문자열의 형식을 지정해서 추가한다.
+
+StringBuilder.prototype.AppendFormat = function() {
+    var count = arguments.length;
+
+    if (count < 2) return '';
+
+    var strValue = arguments[0];
+
+    for (var i = 1; i < count; i++) strValue = strValue.replace('{' + (i - 1) + '}', arguments[i]);
+
+    this.buffer[this.buffer.length] = strValue;
+};
+
+//해당하는 위치에 문자열을 추가한다. (문자위치가 아님)
+
+StringBuilder.prototype.Insert = function(idx, strValue) {
+    this.buffer.splice(idx, 0, strValue);
+};
+
+//해당문자열을 새로운 문자열로 바꾼다.
+
+StringBuilder.prototype.Replace = function(from, to) {
+    for (var i = this.buffer.length - 1; i >= 0; i--) this.buffer[i] = this.buffer[i].replace(new RegExp(from, 'g'), to);
+};
+
+//문자열로 반환한다.
+
+StringBuilder.prototype.ToString = function() {
+    return this.buffer.join('');
+};
+//////////////////////
+
 function solution(number, k) {
-    var answer = '';
     let numArr = number.split('');
-    let numList = [];
+    let len = numArr.length;
     let digit = number.length - k; // 자릿수
 
-    console.log(numArr);
-    while (k) {
-        let max = Math.max.apply(null, numArr.slice(0, numArr.length - k));
-        numArr.splice(0, numArr.indexOf(max.toString()) + 1);
-        answer += max;
-        --k;
+    var answer = new StringBuilder();
 
-        console.log(answer);
-        console.log(numArr);
+    while (digit) {
+        let max, array;
+        if (len === numArr.length && digit != 1) {
+            array = numArr.slice(0, numArr.length - digit);
+        } else {
+            array = numArr.slice(0, numArr.length - digit + 1);
+        }
+        for (var i = 0; i < array.length; i++) {
+            // maxNum 값이 없는 경우 현재 배열값으로 지정
+            if (!max) {
+                max = array[i];
+            }
+
+            // maxNum의 값과 현재 값을 비교해서 maxNum값을 가장 큰 값으로 유지
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+
+        numArr.splice(0, numArr.indexOf(max) + 1);
+        answer.Append(max);
+        digit--;
     }
-
-    answer += numArr;
+    answer = answer.ToString();
     console.log(answer);
-
     return answer;
 }
 
